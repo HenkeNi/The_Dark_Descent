23e735232ff49bbe6f0b502b5719506101ba7a89
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-13 12:48:33 +0200

    Added precompiled header

---
+++ The_Dark_Descent/src/pch.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// Standard library
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <thread>
+
+// Containers
+#include <stack>
+#include <vector>
+#include <unordered_map>
