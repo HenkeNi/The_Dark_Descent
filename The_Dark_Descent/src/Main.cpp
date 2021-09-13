22037d4c83a14a3164c077481da9e29b5ee21330
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-13 16:37:24 +0200

    running game

--- The_Dark_Descent/src/Main.cpp
+++ The_Dark_Descent/src/Main.cpp
@@ -1,8 +1,21 @@
 #include "pch.h"
+#include "Game.h"
 
 
 int main()
 {
-	std::cout << "Hello";
+	std::srand(static_cast<unsigned int>(std::time(nullptr)));
+	std::rand();
+	
+	try
+	{
+		Game game;
+		game.run();
+	}
+	catch (...)
+	{
+		std::cerr << "Unexpected error. Program terminating...\n";
+	}
+
 	return 0;
 }
