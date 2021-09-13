50b8691c2e51466bc4f3436fdb2024d51fa9e147
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-13 17:28:00 +0200

    Monster class - derives from Creature

--- The_Dark_Descent/src/creatures/Monster.cpp
+++ The_Dark_Descent/src/creatures/Monster.cpp
@@ -1,2 +1,8 @@
 #include "pch.h"
 #include "Monster.h"
+
+
+Monster::Monster(Type type)
+	: myType{ type }
+{
+}
--- The_Dark_Descent/src/creatures/Monster.h
+++ The_Dark_Descent/src/creatures/Monster.h
@@ -1,5 +1,18 @@
 #pragma once
-class Monster
+#include "Creature.h"
+
+class Monster : public Creature
 {
+public:
+	enum class Type
+	{
+		Zombie, Vampire, Ghost, Goblin, Imp
+	};
+
+public:
+	Monster(Type type);
+
+private:
+	Type myType;
 };
 
