e1316d06f3fb4b0ca3ac5349089f01e536d034f7
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-13 16:51:17 +0200

    Stats class - contains all the creatures stats

---
+++ The_Dark_Descent/src/creatures/Stats.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+class Stats
+{
+private:
+	int myLife;
+	int myMana;
+	int myArmor;
+	int myHitChance;
+	int myDamageOutput;
+	int myResistFire, myResistLightning, myResistMagic;
+};
+
