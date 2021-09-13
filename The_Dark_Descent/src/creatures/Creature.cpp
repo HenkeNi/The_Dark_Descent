f5247c139f9b75689ffd61618197257ca529bdcd
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-13 17:28:15 +0200

    Creature class - contains logic for fighting

--- The_Dark_Descent/src/creatures/Creature.cpp
+++ The_Dark_Descent/src/creatures/Creature.cpp
@@ -1,2 +1,53 @@
 #include "pch.h"
 #include "Creature.h"
+
+
+void Creature::attack(Creature& someOther) const
+{
+	int damageDealt = getDamageOutput();
+	someOther.takeDamage(damageDealt);
+}
+
+void Creature::takeDamage(int anAmount)
+{
+	myHitPoints -= anAmount;
+
+	if (myHitPoints < 0) { myHitPoints = 0; } 
+}
+
+int Creature::getDamageOutput() const
+{
+	return 10;
+}
+
+
+
+
+void Creature::incrementLevel()
+{
+	++myLevel;
+	// increment stats?
+}
+
+
+
+
+int Creature::getMaxHealth() const
+{
+	return myAttributes.myVitality * 100; // FIX magic number...
+}
+
+
+bool Creature::isAlive() const
+{
+	return myHitPoints > 0;
+}
+
+
+
+
+
+void Creature::printAttributes() const
+{
+
+}
--- The_Dark_Descent/src/creatures/Creature.h
+++ The_Dark_Descent/src/creatures/Creature.h
@@ -1,5 +1,32 @@
 #pragma once
+#include "Attributes.h"
+
 class Creature
 {
+public:
+	
+
+	void attack(Creature& someOther) const;
+	void takeDamage(int anAmount);
+
+	int getDamageOutput() const; // TODO: expensive to calculate every time?
+
+	void incrementLevel();
+
+	int getMaxHealth() const;
+	bool isAlive() const;
+
+private:
+	void printAttributes() const;
+
+private:
+	Attributes myAttributes; 
+	int myHitPoints = 100;
+	int myMagicPoints;
+
+
+	int myLevel;
+	// TODO: carried items...
+
 };
 
