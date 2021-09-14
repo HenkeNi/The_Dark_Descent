ba0fc780ba2a800db5583a2dc4c6965c7fa41b99
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-14 17:11:41 +0200

    skeleton functions added

--- The_Dark_Descent/src/dungeon/Door.cpp
+++ The_Dark_Descent/src/dungeon/Door.cpp
@@ -1,2 +1,15 @@
 #include "pch.h"
 #include "Door.h"
+
+
+
+Door::Door(Room& aFirstRoom, Room& aSecondRoom)
+	: myAdjacentRooms{ std::make_pair(aFirstRoom, aSecondRoom) }
+{
+}
+
+
+Room& Door::openDoor()
+{
+	return myAdjacentRooms.second; // ??
+}
--- The_Dark_Descent/src/dungeon/Door.h
+++ The_Dark_Descent/src/dungeon/Door.h
@@ -1,5 +1,19 @@
 #pragma once
+
+//class Room;
+#include "Room.h"
+
 class Door
 {
+public:
+	Door(Room& aFirstRoom, Room& aSecondRoom);
+
+	Room& openDoor();
+	//void walkThroughDoor(Room& from, Room& to);
+
+
+private:
+	std::pair<Room, Room> myAdjacentRooms;
+
 };
 
