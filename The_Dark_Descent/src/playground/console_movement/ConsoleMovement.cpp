9542df8b024ea7cb2e1164fb5db647ae1e939fc6
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-14 17:13:31 +0200

    placed test code in namespace

---
+++ The_Dark_Descent/src/playground/console_movement/ConsoleMovement.cpp
@@ -0,0 +1,160 @@
+#include "pch.h"
+#include "ConsoleMovement.h"
+
+
+
+
+
+
+namespace playground_movement
+{
+
+
+
+
+	std::vector<std::vector<char>> map
+	{
+		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
+	};
+
+	bool shouldUpdate = true;
+	std::pair<int, int> playerPosition;
+
+
+
+
+	void handleInput()
+	{
+
+		// write: direction to walk:    - ([Enter] to confirm)
+		shouldUpdate = false;
+
+		int input;
+		input = _getch();
+
+		switch (input)
+		{
+		case KEY_W:
+			if (playerPosition.first > 0)
+			{
+				--playerPosition.first;
+				shouldUpdate = true;
+			}
+			break;
+		case KEY_S:
+			if (playerPosition.first < 10)
+			{
+				++playerPosition.first;
+				shouldUpdate = true;
+			}
+			break;
+		case KEY_A:
+			if (playerPosition.second > 0)
+			{
+				--playerPosition.second;
+				shouldUpdate = true;
+			}
+			break;
+		case KEY_D:
+			if (playerPosition.second < 10)
+			{
+				++playerPosition.second;
+				shouldUpdate = true;
+			}
+			break;
+		}
+	}
+
+
+
+	void drawMap()
+	{
+		if (!shouldUpdate)
+			return;
+
+		system("CLS");
+		for (int i{ 0 }; i < 10; ++i)
+		{
+			for (int j{ 0 }; j < 10; ++j)
+			{
+				std::cout << map.at(i).at(j);
+			}
+			std::cout << "\n";
+		}
+
+		std::cout << "\n\n"
+			"Player: o\n"
+			"Chest: x\n";
+
+		//std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+
+
+	void render()
+	{
+		while (true)
+		{
+			drawMap();
+		}
+	}
+
+	void processInput()
+	{
+		while (true)
+		{
+			handleInput();
+
+		}
+	}
+
+	void update()
+	{
+		while (true)
+		{
+
+			/*for (int i = 0; i < 10; ++i)
+			{
+				for (int j = 0; j < 10; ++j)
+				{
+					if (i == playerPosition.first && j == playerPosition.second)
+						map[i][j] == 'o';
+					else
+						map[i][j] == '#';
+				}
+			}*/
+			map.at(playerPosition.first).at(playerPosition.second) = 'o';
+		}
+	}
+
+
+
+	void run()
+	{
+
+		playerPosition = std::make_pair<int, int>(0, 0);
+
+		std::thread renderThread(render);
+		std::thread inputThread(processInput);
+		std::thread updateThread(update);
+
+		renderThread.join();
+		inputThread.join();
+		updateThread.join();
+
+		std::cout << "Finished!\n";
+
+	}
+
+
+
+}
+
---
+++ The_Dark_Descent/src/playground/console_movement/ConsoleMovement.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define KEY_W 119
+#define KEY_A 97
+#define KEY_S 115
+#define KEY_D 100
+
+
+
+namespace playground_movement
+{
+
+
+
+	void handleInput();
+	void drawMap();
+
+	void render();
+	void processInput();
+	void update();
+
+	void run();
+
+}
