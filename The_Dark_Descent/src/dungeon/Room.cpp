cbed8cd8a3cb0e2ded3d68a8fb542facec79a035
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-14 14:25:46 +0200

    expanded the Room class with logic for fighting enemy
    Move later !

--- The_Dark_Descent/src/dungeon/Room.cpp
+++ The_Dark_Descent/src/dungeon/Room.cpp
@@ -1,2 +1,73 @@
 #include "pch.h"
 #include "Room.h"
+
+#include "Hero.h"
+#include "Monster.h"
+#include "Utility.h"
+
+
+Room::Room(Hero& aPlayer)
+	: myPlayer{ aPlayer }
+{
+	initMonsters();
+}
+
+
+void Room::initMonsters()
+{
+	// TODO: create monsters -> maybe have factory...
+	myMonsters.push({ Monster::Type::Vampire });
+
+}
+
+
+
+// Player choses what action to take 
+void Room::getInteraction() 
+{
+
+	if (Utility::getRandomNumber(1, 6) % 2 == 0)
+	{
+		encounterEnemy();
+	}
+}
+
+
+void Room::encounterEnemy()
+{
+	Monster& monster = myMonsters.top();
+	std::cout << "Something moves in the shadows! A " << monster.getTypeAsString() << " appears\n";
+
+	// TODO: pick an option... (run, fight, etc)
+
+
+	// If win -> pop() stack...
+
+}
+
+
+void Room::battle(Hero& aPlayer, Monster& aMonster)
+{
+	aMonster.attack(aPlayer);
+	aMonster.attack(aPlayer);
+
+	while (aPlayer.isAlive() && aMonster.isAlive())
+	{
+		aPlayer.attack(aMonster); // or chose an option..? - ASK FOR INPUT? (1. attack, 2. run away (escape), 3.
+
+		if (aMonster.isAlive())
+		{
+			aMonster.attack(aPlayer);
+		}
+		else
+		{
+			std::cout << "Player wins!";
+			aPlayer.loot(aMonster); // Take gold, check if items to keep?!
+			return;
+		}
+	}
+
+	std::cout << "Enemy wins!\n";
+
+
+}
--- The_Dark_Descent/src/dungeon/Room.h
+++ The_Dark_Descent/src/dungeon/Room.h
@@ -1,5 +1,27 @@
 #pragma once
+#include "Monster.h"
+
+class Hero;
+
 class Room
 {
+public:
+	Room(Hero& aPlayer);
+
+
+	void getInteraction(); // TODO: rename!? 
+
+private:
+	void initMonsters();
+	
+	void encounterEnemy();
+	void battle(Hero& aPlayer, Monster& aMonster);
+
+private:
+	Hero& myPlayer;
+	// FIGHT MANAGER?? - BattleManager (singleton?!)
+	
+	std::stack<Monster> myMonsters;
+
 };
 
