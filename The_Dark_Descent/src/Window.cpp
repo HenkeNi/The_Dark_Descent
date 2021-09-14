5039d3a6026f481fa60584df4846a39683337e19
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-14 14:27:50 +0200

    unused window class

---
+++ The_Dark_Descent/src/Window.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "Window.h"
+
+
+
+void Window::changeColor(Color aColor)
+{
+		
+	//HANDLE consoleColor;
+	//consoleColor = GetStdHandle(STD_OUTPUT_HANDLE);
+
+
+	
+	switch (aColor)
+	{
+	case Window::Color::DarkRed:
+		//SetConsole
+		break;
+	case Window::Color::DarkGray:
+		break;
+	case Window::Color::White:
+		break;
+	}
+}
+
+
+void Window::resetColor()
+{
+
+}
---
+++ The_Dark_Descent/src/Window.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+class Window
+{
+public:
+	enum class Color
+	{
+		DarkRed, DarkGray, White
+	};
+
+public:
+	void changeColor(Color aColor);
+	void resetColor();
+
+
+private:
+
+};
+
