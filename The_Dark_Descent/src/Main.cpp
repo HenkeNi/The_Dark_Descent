8966cc739fd99cd9ffb004cfa0dff4a525968938
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-14 17:13:17 +0200

    moved test code

--- The_Dark_Descent/src/Main.cpp
+++ The_Dark_Descent/src/Main.cpp
@@ -19,197 +19,21 @@
 
 
 
-std::vector<std::vector<char>> map
-{
-	{'o', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-	{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-	{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-	{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-	{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-	{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-	{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-	{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-	{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-	{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
-};
-
-std::pair<int, int> playerPosition;
-
-void handleInput()
-{
-
-	// write: direction to walk:    - ([Enter] to confirm)
-
-
-	char c;
-	//std::cin >> c;
-	scanf_s("%c", &c);
-
-	switch (c)
-	{
-	case 'w':
-		if (playerPosition.first > 0)
-		{
-			--playerPosition.first;
-		}
-		break;
-	case 's':
-		if (playerPosition.first < 10)
-		{
-			++playerPosition.first;
-		}
-		break;
-	case 'a':
-		if (playerPosition.second > 0)
-		{
-			--playerPosition.second;
-		}
-		break;
-	case 'd':
-		if (playerPosition.second < 10)
-		{
-			++playerPosition.second;
-		}
-		break;
-	}
-}
-
-void drawMap()
-{
-	system("CLS");
-	for (int i{ 0 }; i < 10; ++i)
-	{
-		for (int j{ 0 }; j < 10; ++j)
-		{
-			std::cout << map.at(i).at(j);
-		}
-		std::cout << "\n";
-	}
-
-	std::cout << "\n\n"
-		"Player: o\n"
-		"Chest: x\n";
-
-	std::this_thread::sleep_for(std::chrono::seconds(1));
-}
-
-
-void render()
-{
-	while (true)
-	{
-		drawMap();
-	}
-}
-
-void processInput()
-{
-	while (true)
-	{
-		handleInput();
-
-	}
-}
-
-void update()
-{
-	while (true)
-	{
-
-		/*for (int i = 0; i < 10; ++i)
-		{
-			for (int j = 0; j < 10; ++j)
-			{
-				map.at(i).at(j) = '#';
-			}
-		}*/
-		map.at(playerPosition.first).at(playerPosition.second) = 'o';
-
-	}
-}
-
-
-
-
-
-
-
-
-
+//#include <windows.h>
+#include "playground/console_movement/ConsoleMovement.h"
 
 int main()
 {
+	playground_movement::run();
 
 
-	playerPosition = std::make_pair<int, int>(0, 0);
-
-	std::thread renderThread(render);
-	std::thread inputThread(processInput);
-	std::thread updateThread(update);
-
-	renderThread.join();
-	inputThread.join();
-	updateThread.join();
-
-	std::cout << "Finished!\n";
-
-	/*char characterMap[10][10]
-	{
-		{'o', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-		{'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
-	};*/
+	//SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED |
+	//	FOREGROUND_GREEN |
+	//	FOREGROUND_INTENSITY |
+	//	BACKGROUND_BLUE
+	//);
 
 
-	//while (true)
-	//{
-	//	for (int i{ 0 }; i < 10; ++i)
-	//	{
-	//		for (int j{ 0 }; j < 10; ++j)
-	//		{
-	//			std::cout << characterMap[i][j];
-	//			
-	//		}
-	//		std::cout << '\n';
-	//	}
-
-	//	// TODO: multitreading...
-
-	//	char c;
-	//	std::cin >> c;
-	//	if (c == 's')
-	//	{
-	//		characterMap[0][0] = '#';
-	//		characterMap[1][0] = 'o';
-	//	}
-
-	//	system("CLS");
-	//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//HANDLE consoleColor;
-	//consoleColor = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	//SetConsoleTextAttribute(consoleColor, 1);
 
 	std::srand(static_cast<unsigned int>(std::time(nullptr)));
 	std::rand();
