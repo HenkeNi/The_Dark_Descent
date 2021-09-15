9fba9f3d95f9348e6e3ea8f2564003d57604f0bc
Henrik Jangefelt <henke9105@hotmail.com>
2021-09-15 16:25:18 +0200

    some experimenting with Rooms

--- The_Dark_Descent/src/Main.cpp
+++ The_Dark_Descent/src/Main.cpp
@@ -20,11 +20,60 @@
 
 
 //#include <windows.h>
-#include "playground/console_movement/ConsoleMovement.h"
+//#include "playground/console_movement/ConsoleMovement.h"
+
+
+#include "Room.h"
+#include "Door.h"
+#include "Hero.h"
+
+
+
+/*
+*	Player keeps track of which rooms his in??
+*		also have logic for open Doors?? - gets it from the Room* in Hero... 
+*		
+*	Hero:
+*		OpenDoor()
+*		
+* 
+* 
+*/
+
+
+
+
 
 int main()
 {
-	playground_movement::run();
+
+	Hero hero{ Hero::Type::Human };
+
+	Room room1;
+	Room room2;
+	room1.setPlayer(&hero);
+	room1.addDoor({ &room1, &room2 });
+	room2.setPlayer(nullptr);
+
+	std::cout << "Player is in Room 1 ?" << (room1.getPlayer() != nullptr ? true : false) << '\n';
+	std::cout << "Player is in Room 2 ?" << (room2.getPlayer() != nullptr ? true : false) << '\n';
+
+	hero.updateCurrentRoom(&room1);
+	hero.openDoor(room1.getDoor(0));
+
+	std::cout << "Player is in Room 1 ?" << (room1.getPlayer() != nullptr ? true : false) << '\n';
+	std::cout << "Player is in Room 2 ?" << (room2.getPlayer() != nullptr ? true : false) << '\n';
+
+
+
+
+	system("pause");
+	
+
+
+
+
+	//playground_movement::run();
 
 
 	//SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED |
